47e9ffa54eae98f075aa5ab8cd7ff5fa21084f2d
jlnshly <jelaineashleyramos@gmail.com>
2026-04-27 17:17:22 +0800

    reading file code

--- file_manager.py
+++ file_manager.py
@@ -2,4 +2,10 @@ import os
 
 class FileManager:
     @staticmethod
-    def read_file():
+    def read_file(filepath):
+        """Reads integers from file"""
+        if not os.path.exists(filepath):
+            return []
+        with open(filepath, 'r') as f:
+            return [int(x) for x in f.read().split() if x.lstrip('-').isdigit()]
+
