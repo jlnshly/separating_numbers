6d34de2359db058132971d5e46f35c7436204f64
jlnshly <jelaineashleyramos@gmail.com>
2026-04-27 17:26:08 +0800

    coding final functions

--- main_program.py
+++ main_program.py
@@ -2,3 +2,17 @@ from file_manager import FileManager
 from functionalities import NumberSeparator
 import time
 
+def run_transmuter():
+    source = 'integers.txt'
+    even_numbers = 'double.txt'
+    odd_numbers = 'triple.txt'
+    print('Welcome...')
+    time.sleep(1)
+    raw_data = FileManager.read_file(source)
+    if not raw_data:
+        print(f'No data available, {source} might be missing/empty!')
+        return
+    engine = NumberSeparator(raw_data)
+    squared
+
+
