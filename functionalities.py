e245ee9638dad20066a7c919804f1f50868257c7
jlnshly <jelaineashleyramos@gmail.com>
2026-04-27 17:12:19 +0800

    progress

--- functionalities.py
+++ functionalities.py
@@ -2,3 +2,12 @@ class NumberSeparator:
     """Transmuting numbers based on their parity"""
     def __init__(self, number_list):
         self.numbers = number_list
+
+    def transmute_even(self):
+        """Squares even numbers"""
+        return [x ** 2 for x in self.numbers if x % 2 == 0]
+
+    def transmute_odd(self):
+        """Cubes odd numbers"""
+        return [x ]
+
