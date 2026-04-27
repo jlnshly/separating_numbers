308dd24cafad09476f9604d97e3f0561a796f29f
jlnshly <jelaineashleyramos@gmail.com>
2026-04-27 17:28:55 +0800

    coding displays

--- main_program.py
+++ main_program.py
@@ -15,5 +15,10 @@ def run_transmuter():
     engine = NumberSeparator(raw_data)
     squared_evens = engine.transmute_even()
     cubed_odds = engine.transmute_odd()
+    FileManager.write_file(even_numbers, squared_evens)
+    FileManager.write_file(odd_numbers, cubed_odds)
+    print('Transmutation successful!')
+    print(f'{len(squared_evens)} squared evens were transmitted to {even_numbers}!')
+    print()
 
 
